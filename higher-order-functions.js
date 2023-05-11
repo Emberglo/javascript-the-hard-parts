af2e3c15bd9e9f86f64b8ef6b124c449a23c3f2c
Ryan Carpenter <rccarpenter@outlook.com>
2023-05-11 16:09:18 -0600

    higher order function example

---
+++ higher-order-functions.js
@@ -0,0 +1,18 @@
+// Higher Order Functions Example:
+
+// This is the higher order function:
+function copyArrayAndManipulate(array, instructions) {
+    const output = [];
+
+    for(let i = 0; i < array.length; i++) {
+        output.push(instructions(array[i]));
+    }
+
+    return output;
+}
+
+//These are the callback functions that can be passed in to the higher order function to change how the array is manipulated
+function multiplyBy2(input) { return input * 2; }
+function divideBy2(input) { return input / 2; }
+
+const result = copyArrayAndManipulate([1, 2, 3], multiplyBy2);
