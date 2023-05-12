5f3b26e9e79fd0218269a2b86648d84525a1c7e9
Ryan Carpenter <rccarpenter@outlook.com>
2023-05-12 11:47:03 -0600

    adding arrow function examples

--- higher-order-functions.js
+++ higher-order-functions.js
@@ -13,10 +13,15 @@ function copyArrayAndManipulate(array, instructions) {
     return output;
 }
 
-//These are the callback functions that can be passed in to the higher order function to change how the array is manipulated
+// These are the callback functions that can be passed in to the higher order function to change how the array is manipulated
 function multiplyBy2(input) { return input * 2; }
+// Can use arrow functions here
+const times3 = input => input*3;
 function divideBy2(input) { return input / 2; }
 function add2(input) { return input + 2; }
 function subtract2(input) { return input - 2; }
 
 const result = copyArrayAndManipulate([1, 2, 3], multiplyBy2);
+
+// Can pass a whole arrow function as a parameter into a function call
+const arrowResult = copyArrayAndManipulate([3, 4, 5], input => input*3);
