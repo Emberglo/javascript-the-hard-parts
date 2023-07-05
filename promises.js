ea875852aacc0616fcd999b4a1d2198b6f22a834
Ryan Carpenter <rccarpenter@outlook.com>
2023-07-05 15:08:11 -0600

    Adding Notes

--- promises.js
+++ promises.js
@@ -1,9 +1,15 @@
+// Saves display function in global memory
 function display(data) {
     console.log(data);
 }
 
+// fetch creates a promise object and begins the network request to the given link
 const futureData = fetch('https://jsonplaceholder.typicode.com/todos/1');
 
+// .then adds the display function to the onFulfilled in the promise object to be run when the data returns from the request
 futureData.then(display);
 
+// first thing to happen in the console
 console.log("Me First!")
+
+// after the Me First and after the data returns from the fetch, display runs and prints out the response data
