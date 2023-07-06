cfa309b47ce024b5c3e77a4ba3622eea36fdf376
Ryan Carpenter <rccarpenter@outlook.com>
2023-07-06 12:19:20 -0600

    classes and prototypes and oop

---
+++ classes-and-prototypes.js
@@ -0,0 +1,11 @@
+// objects allow us to store data and functionality together - principle of ENCAPSULATION
+
+const user1 = {
+    name: "Will",
+    score: 3,
+    increment: function() { user1.score++; }
+};
+
+// to increment the user's score:
+user1.increment();
+
